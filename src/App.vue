bc248bb14c04b1df07279d083d358c4a460641c3
crist <crist.soares2013@live.com>
2022-12-23 22:20:43 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <main>
+    </main>
+</template>
+
+<style lang="scss">
+main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width: 100vw;
+    background-color: #333;
+}
+</style>
